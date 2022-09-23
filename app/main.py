3d2820d519b1282d2060ed9ee23cf4ec50b66ed9
gziz <69287011+gziz@users.noreply.github.com>
2022-09-23 10:24:00 -0500

    First commit

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from os import getcwd
+import requests
+#from haystack.document_stores import ElasticsearchDocumentStore
+
+
+@app.get("/")
+def read_root():
+
+    res = requests.get('http://localhost:9200/_cat/indices').text
+    return {"message": res}
+
