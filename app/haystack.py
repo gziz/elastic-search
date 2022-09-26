e2858fa5a7f79ff9d3f5b28b77c8d362318ff303
gziz <69287011+gziz@users.noreply.github.com>
2022-09-25 20:14:53 -0500

    temp

--- app/haystack.py
+++ app/haystack.py
@@ -1,7 +1,7 @@
 from haystack.document_stores import ElasticsearchDocumentStore
-from haystack.nodes import BM25Retriever
-from haystack.nodes import FARMReader
-from haystack.pipelines import ExtractiveQAPipeline
+# from haystack.nodes import BM25Retriever
+# from haystack.nodes import FARMReader
+# from haystack.pipelines import ExtractiveQAPipeline
 import pathlib
 
 
